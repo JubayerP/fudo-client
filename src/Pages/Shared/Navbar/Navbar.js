435190f1881d4d450258ef9b7d04ba8a98e9e2ba
JubayerDev <ajashes858@gmail.com>
2022-12-17 22:59:23 +0600

    navbar added

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,30 +1,39 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import logo from '../../../assets/logo.png';
 
 const Navbar = () => {
     const menuItems = <>
-        <Link></Link>
+        <NavLink to='/'><li>Home</li></NavLink>
+        <NavLink to='/services'><li>Services</li></NavLink>
+        <NavLink to='/'><li>Home</li></NavLink>
+        <NavLink to='/'><li>Home</li></NavLink>
+        <NavLink to='/'><li>Home</li></NavLink>
     </>
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar no-animation">
   <div className="navbar-start">
     <div className="dropdown">
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
-      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        
+      <ul tabIndex={0} className="menu text-secondary menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        {menuItems}
       </ul>
     </div>
-    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <Link to='/'>
+                    <img src={logo} alt="" />
+    </Link>
   </div>
   <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-                    
+    <ul className="space-x-8 menu-horizontal px-1 text-secondary text-lg">
+           {menuItems}
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Get started</a>
+    <Link to='/'>
+        <button className="btn bg-primary hover:bg-primary border-0 px-8 rounded-full text-white">Sign In</button>
+    </Link>
   </div>
 </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,16 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#EA3F30",
+          "secondary": "#2E2E2E",
+          "accent": "#FEF5F5",
+          
+        }
+      }
+    ]
+  },
 }
