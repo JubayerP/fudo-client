7edd77e04a9cec242d1b9dc80dcf4c61a53ebae3
JubayerDev <ajashes858@gmail.com>
2022-12-20 15:12:14 +0600

    dis

--- src/Pages/Home/Discount/Discount.js
+++ src/Pages/Home/Discount/Discount.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Discount = () => {
     return (
         <div className='my-20 container mx-auto'>
-            <div className='flex justify-between items-center mb-14'>
+            <div className='flex lg:flex-row flex-col space-y-10 justify-between items-center mb-14'>
                 <div>
-                    <h1 className="text-6xl font-bold text-secondary">Popular Discount</h1>
+                    <h1 className="lg:text-6xl text-4xl font-bold text-secondary">Popular Discount</h1>
                 </div>
                 <div>
                     <div className='flex items-center'>
@@ -17,28 +17,28 @@ const Discount = () => {
             </div>
 
 
-            <div className='flex gap-10 items-center'>
+            <div className='flex lg:flex-row flex-col gap-10 items-center'>
                 <div className='bg-accent px-4 py-4 rounded-lg'>
-                    <h3 className="text-4xl font-bold text-secondary mb-4">Cheese Stuffed Burgers</h3>
-                    <p className="text-xl text-secondary mb-6">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy</p>
+                    <h3 className="text-4xl font-bold text-secondary mb-4 p-4">Cheese Stuffed Burgers</h3>
+                    <p className="text-xl text-secondary mb-6 p-4">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy</p>
 
-                    <div className='flex justify-between'>
+                    <div className='flex justify-between p-4'>
                         <h4 className='font-bold text-primary text-3xl'>$8.99</h4>
                         <button className="btn bg-primary hover:bg-primary border-0 px-8 rounded-full text-white capitalize no-animation">Order Now</button>
                     </div>
 
-                    <img className='' src="https://templatekit.tokomoo.com/deliverykit/wp-content/uploads/sites/26/2021/07/burger-95CWFUB-removebg.png" alt="" />
+                    <img className='mx-auto lg:w-[91%] w-1/2' src="https://templatekit.tokomoo.com/deliverykit/wp-content/uploads/sites/26/2021/07/burger-95CWFUB-removebg.png" alt="" />
                 </div>
                 <div className='bg-accent px-4 py-4 rounded-lg'>
-                    <h3 className="text-4xl font-bold text-secondary mb-4">Cheese Stuffed Burgers</h3>
-                    <p className="text-xl text-secondary mb-4">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy</p>
+                    <h3 className="text-4xl font-bold text-secondary mb-4 p-4">Cheese Stuffed Burgers</h3>
+                    <p className="text-xl text-secondary mb-4 p-4">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy</p>
 
-                    <div className='flex justify-between'>
+                    <div className='flex justify-between p-4'>
                         <h4 className='font-bold text-primary text-3xl'>$4.99</h4>
                         <button className="btn bg-primary hover:bg-primary border-0 px-8 rounded-full text-white capitalize no-animation">Order Now</button>
                     </div>
 
-                    <img className='w-[93%]' src="https://templatekit.tokomoo.com/deliverykit/wp-content/uploads/sites/26/2021/07/plate-of-asian-food-GF8U9TQ-removebg.png" alt="" />
+                    <img className='lg:w-[93%] w-1/2 mx-auto' src="https://templatekit.tokomoo.com/deliverykit/wp-content/uploads/sites/26/2021/07/plate-of-asian-food-GF8U9TQ-removebg.png" alt="" />
                 </div>
             </div>
         </div>
